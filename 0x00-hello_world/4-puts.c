7277b185c337f2bde65554229a16818d6e156379
Munro617 <marimucivilcontractors@gmail.com>
2022-09-09 13:32:04 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include  <stdio.h>
+
+/** 
+* main - print the string inout function
+*
+* Description: using the main function 
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0 
+*/
+int main(void)
+{
+         puts("/"Programming is like building a multilingual puzzle ");
+         return (0);
+}
